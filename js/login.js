9fbcaf83e70e54ef38eb813603980d7aa55f76d7
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-15 14:30:14 +0900

    feat: 로그인 버튼 누르면 로그인된 메인 페이지로 이동하기

---
+++ js/login.js
@@ -0,0 +1,7 @@
+const loginButton = document.getElementsByClassName('login-button')[0];
+
+loginButton.onclick = () => {
+    // TODO: 로그인 로직 구현
+    const userId = 'hyeg0121';
+    window.open(`../main?id=${userId}`, '_top');
+};
