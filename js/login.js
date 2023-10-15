47347d0db4ed276b383c73360ed954ad0f3ca23a
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-15 15:57:25 +0900

    fix: id 쿼리스트링 추가

--- js/login.js
+++ js/login.js
@@ -3,5 +3,5 @@ const loginButton = document.getElementsByClassName('login-button')[0];
 loginButton.onclick = () => {
     // TODO: 로그인 로직 구현
     const userId = 'hyeg0121';
-    window.open(`../main?id=${userId}`, '_top');
+    window.open(`../main/?id=${userId}`, '_top');
 };
