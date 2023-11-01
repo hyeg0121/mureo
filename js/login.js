7e4a710c2490fd2b08efb0c21eca2f41d4642948
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-01 20:46:39 +0900

    feat: 로그인 api 요청

--- js/login.js
+++ js/login.js
@@ -1,7 +1,22 @@
+const idField = document.getElementsByClassName('id-field')[0];
+const pwField = document.getElementsByClassName('pw-field')[0];
 const loginButton = document.getElementsByClassName('login-button')[0];
 
 loginButton.onclick = () => {
-    // TODO: 로그인 로직 구현
-    const userId = 'hyeg0121';
-    window.open(`../main/?id=${userId}`, '_top');
+    const id = idField.value;
+    const pw = pwField.value;
+
+    const req = {
+        "user_id": id,
+        "password": pw
+    };
+
+    axios.post('http://localhost:3000/login', req)
+        .then(res => {
+            localStorage.setItem('user', JSON.stringify(res));
+            window.open('../main/', '_top');
+        })
+        .catch(error => {
+            console.log(error)
+        })
 };
