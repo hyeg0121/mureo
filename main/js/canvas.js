2dc50d500d2db623d06363f75fbe8d2c2f7813fc
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-08 00:39:42 +0900

    feat: 차트에 데이터 반영, 차트 타입 수정

--- main/js/canvas.js
+++ main/js/canvas.js
@@ -6,7 +6,7 @@ let colors = [];
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 function resizeCanvas() {
-    const containerWidth = canvasContainer.clientWidth; 
+    const containerWidth = canvasContainer.clientWidth;
     const containerHeight = canvasContainer.clientHeight;
 
     canvas.width = containerWidth;
@@ -19,37 +19,77 @@ console.log(interestColors, interestLabels, interestPostCounts);
 function rgbToRgba(rgbCode, alpha) {
     // RGB 코드에서 # 제거
     rgbCode = rgbCode.replace(/^#/, '');
-  
+
     // R, G, B 값 추출
     const r = parseInt(rgbCode.substring(0, 2), 16);
     const g = parseInt(rgbCode.substring(2, 4), 16);
     const b = parseInt(rgbCode.substring(4, 6), 16);
-  
+
     // RGBA 문자열 반환
     return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 }
 
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: interestLabels,
-        type: 'bar',
-        datasets: [{
-            label: '관심사',
-            data: [3, 1, 2],
-            backgroundColor: [...interestColors],
-            borderColor: [...interestColors],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+async function drawChart() {
+    await getUsersInterest();
 
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: interestLabels.reverse(),
+            type: 'line',
+            datasets: [{
+                label: '관심사',
+                data: interestPostCounts,
+                backgroundColor: rgbToRgba(interestColors, 1),
+                borderColor: ['rgba(37, 168, 224, 1)'],
+                borderWidth: 5
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false,
+                    labels: {
+                        font: {
+                            size: 14,
+                            family: 'Pretendard-Regular'
+                        }
+                    }
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            title: {
+                display: true,
+                text: '관심사 분석',
+              }
+        }
+    });
+
+}
+
+drawChart();
+
+
+function rgbToRgba(rgbCodes, alpha) {
+    let colors = [];
+    for (let rgbCode of rgbCodes) {
+
+        rgbCode = rgbCode.replace(/^#/, '');
+
+        const r = parseInt(rgbCode.substring(0, 2), 16);
+        const g = parseInt(rgbCode.substring(2, 4), 16);
+        const b = parseInt(rgbCode.substring(4, 6), 16);
+
+        // RGBA 문자열 반환
+        console.log(`rgba(${r}, ${g}, ${b}, ${alpha})`);
+        colors.push(`rgba(${r}, ${g}, ${b}, ${alpha})`);
+    }
+
+    return colors;
+}
 window.addEventListener('load', resizeCanvas);
 window.addEventListener('resize', resizeCanvas);
