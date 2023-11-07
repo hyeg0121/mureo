180f45ba09a008657c6efc8c6815b451d43e5203
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-07 23:26:10 +0900

    feat: 차트 데이터 불러오기

--- main/js/canvas.js
+++ main/js/canvas.js
@@ -2,50 +2,44 @@ const canvas = document.getElementById("chart");
 const canvasContainer = document.getElementsByClassName("canvas-containter")[0];
 
 const ctx = canvas.getContext("2d");
+let colors = [];
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 function resizeCanvas() {
     const containerWidth = canvasContainer.clientWidth; 
     const containerHeight = canvasContainer.clientHeight;
 
-    console.log(containerWidth, containerHeight);
     canvas.width = containerWidth;
     canvas.height = containerHeight;
 
-    drawOnCanvas();
 }
 
-function drawOnCanvas() {
-    // Canvas에 그리고자 하는 내용을 그리는 코드를 여기에 작성
+console.log(interestColors, interestLabels, interestPostCounts);
+
+function rgbToRgba(rgbCode, alpha) {
+    // RGB 코드에서 # 제거
+    rgbCode = rgbCode.replace(/^#/, '');
+  
+    // R, G, B 값 추출
+    const r = parseInt(rgbCode.substring(0, 2), 16);
+    const g = parseInt(rgbCode.substring(2, 4), 16);
+    const b = parseInt(rgbCode.substring(4, 6), 16);
+  
+    // RGBA 문자열 반환
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 }
 
-const ctx_ = canvas.getContext('2d');
-const myChart = new Chart(ctx_, {
+const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        label: '',
-        labels: ['나는 개발이 좋다', 'NC 다이노스', '오일 파스텔'],
+        labels: interestLabels,
         type: 'bar',
         datasets: [{
-            label: '',
-            data: [3, 1, 1],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 2
+            label: '관심사',
+            data: [3, 1, 2],
+            backgroundColor: [...interestColors],
+            borderColor: [...interestColors],
+            borderWidth: 1
         }]
     },
     options: {
