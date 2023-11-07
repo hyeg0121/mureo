0da3af072936a01b9eb9e3ffa8addccff9d80cfe
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-07 17:41:06 +0900

    refactor: 함수 정리

--- friends/js/friends.js
+++ friends/js/friends.js
@@ -3,8 +3,8 @@ const followerListDiv = document.getElementsByClassName('follower-list')[0];
 const searchField = document.getElementById('search-field');
 const searchIcon = document.getElementsByClassName('search-icon')[0];
 
-getFollowings();
-getFollowers();
+getUsers('followings', followingListDiv);
+getUsers('followers', followerListDiv);
 
 searchField.onkeydown = e => {
     if (e.key === 'Enter') {
@@ -16,11 +16,11 @@ searchIcon.onclick = () => {
     getSearchResult();
 };
 
-async function getFollowings() {
+async function getUsers(kind, listDiv) {
     try {
-        const response = await axios.get(`${BASE_URL}/followings/${userNo}`);
-        const followings = response.data;
-        for (let following of followings) {
+        const response = await axios.get(`${BASE_URL}/${kind}/${userNo}`);
+        const results = response.data;
+        for (let result of results) {
 
             const friendItem = document.createElement('div');
             friendItem.className = 'friend-item';
@@ -42,11 +42,11 @@ async function getFollowings() {
 
             const friendName = document.createElement('div');
             friendName.className = 'friend-name';
-            friendName.textContent = following.user_name;
+            friendName.textContent = result.user_name;
 
             const friendId = document.createElement('div');
             friendId.className = 'friend-id';
-            friendId.textContent = following.user_id;
+            friendId.textContent = result.user_id;
 
             profileLabels.appendChild(friendName);
             profileLabels.appendChild(friendId);
@@ -54,7 +54,7 @@ async function getFollowings() {
             // 프렌드 인터레스트를 생성 
             const friendInterest = document.createElement('div');
             friendInterest.className = 'friend-interest';
-            const friendRecentInterest = await getUsersRecentInterest(following.user_no);
+            const friendRecentInterest = await getUsersRecentInterest(result.user_no);
             console.log(friendRecentInterest);
             friendInterest.textContent = friendRecentInterest;
 
@@ -63,13 +63,13 @@ async function getFollowings() {
             followButton.className = 'follow-button';
             followButton.textContent = '팔로우 취소';
 
-            const isFollow = await isFollowing(userNo, following.user_no);
+            const isFollow = await isFollowing(userNo, result.user_no);
             if (isFollow) {
                 followButton.onclick = () => {
-                    console.log(userNo, following.user_no)
+                    console.log(userNo, result.user_no)
                     axios.delete(`${BASE_URL}/users/unfollow`, {
                         "follower_id": userNo,
-                        "following_id": following.user_no
+                        "following_id": result.user_no
                     })
                         .then(res => {
                             console.log(res.data);
@@ -85,7 +85,7 @@ async function getFollowings() {
                 followButton.onclick = () => {
                     axios.post(`${BASE_URL}/users/follow`, {
                         "follower_id": userNo,
-                        "following_id": following.user_no
+                        "following_id": result.user_no
                     })
                         .then(res => {
                             console.log(res);
@@ -106,109 +106,12 @@ async function getFollowings() {
             friendItem.appendChild(followButton);
 
             // 부모 요소에 추가.
-            followingListDiv.appendChild(friendItem);
+            listDiv.appendChild(friendItem);
         }
     } catch (exception) {
         console.error(exception);
     }
 }
-
-async function getFollowers() {
-    try {
-        const response = await axios.get(`${BASE_URL}/followers/${userNo}`);
-        const followers = response.data;
-        for (let follower of followers) {
-
-            const friendItem = document.createElement('div');
-            friendItem.className = 'friend-item';
-
-            // 프로필 컨테이너를 생성 
-            const friendProfileContainer = document.createElement('div');
-            friendProfileContainer.className = 'friend-profile-container';
-
-            // 프로필 이미지를 생성 
-            const friendImg = document.createElement('div');
-            friendImg.className = 'friend-img';
-            const img = document.createElement('img');
-            img.src = '../image/profile.png';
-            friendImg.appendChild(img);
-
-            // 프로필 레이블을 생성 
-            const profileLabels = document.createElement('div');
-            profileLabels.className = 'profile-labels';
-
-            const friendName = document.createElement('div');
-            friendName.className = 'friend-name';
-            friendName.textContent = follower.user_name;
-
-            const friendId = document.createElement('div');
-            friendId.className = 'friend-id';
-            friendId.textContent = follower.user_id;
-
-            profileLabels.appendChild(friendName);
-            profileLabels.appendChild(friendId);
-
-            // 프렌드 인터레스트를 생성 
-            const friendInterest = document.createElement('div');
-            friendInterest.className = 'friend-interest';
-            const friendRecentInterest = await getUsersRecentInterest(follower.user_no);
-            console.log(friendRecentInterest);
-            friendInterest.textContent = friendRecentInterest;
-
-            // 팔로우 버튼을 생성 
-            const followButton = document.createElement('button');
-            followButton.className = 'follow-button';
-            followButton.textContent = '팔로우 취소';
-
-            // 요소를 조합 
-            friendProfileContainer.appendChild(friendImg);
-            friendProfileContainer.appendChild(profileLabels);
-
-            friendItem.appendChild(friendProfileContainer);
-            friendItem.appendChild(friendInterest);
-            friendItem.appendChild(followButton);
-
-            const isFollow = await isFollowing(userNo, follower.user_no);
-            if (isFollow) {
-                followButton.onclick = () => {
-                    axios.delete(`${BASE_URL}/users/unfollow`, {
-                        "follower_id": userNo,
-                        "following_id": follower.user_no
-                    })
-                        .then(res => {
-                            console.log(res);
-                        })
-                        .catch(err => {
-                            console.error(err);
-                        });
-                    location.reload();
-                }
-            } else {
-                followButton.innerHTML = '팔로우';
-                followButton.classList.add('unfollow')
-                followButton.onclick = () => {
-                    axios.post(`${BASE_URL}/users/follow`, {
-                        "follower_id": userNo,
-                        "following_id": follower.user_no
-                    })
-                        .then(res => {
-                            console.log(res);
-                        })
-                        .catch(err => {
-                            console.error(err);
-                        });
-                    location.reload();
-                }
-            }
-
-            // 부모 요소에 추가.
-            followerListDiv.appendChild(friendItem);
-        }
-    } catch (exception) {
-        console.error(exception);
-    }
-}
-
 
 function getSearchResult() {
     const keyword = searchField.value.trim();
@@ -251,3 +154,4 @@ async function isFollowing(follower_no, following_no) {
 
     return flag;
 }
+
